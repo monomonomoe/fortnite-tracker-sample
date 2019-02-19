f01d015e453ab4b51fde9885a427cd10cc81d00f
anzchaaan <anzchaaan@gmail.com>
2019-02-19 22:00:34 +0900

    jsでの検索機能を実装

---
+++ docs/js/main.js
@@ -0,0 +1,204 @@
+window.onload = (() => {
+    'use strict';
+
+    /**
+     * 学生さんへ
+     * こんなコードまねしちゃ駄目だぞ。
+     * 
+     * エンジニアさんへ
+     * 普段はもうちょっとしっかりしたコード書いてます！
+     * コメントうざいくらい書いてます！サーバーサイドの人間なんです！許してください！
+     * お仕事ください！
+     */
+
+    const urlUserMetaAPI = 'https://fortnite-public-api.theapinetwork.com/prod09/users/id';
+    const urlUserStatusAPI = 'https://fortnite-public-api.theapinetwork.com/prod09/users/public/br_stats_v2';
+    
+    const buttonStart = document.querySelector('#start');
+    const textUserName = document.querySelector('#user_name');
+    const result = document.querySelector('#result');
+    const message = document.querySelector('#message');
+    const playstyle = document.querySelector('#playstyle');
+
+    const twitter_listdocument = document.querySelector("#twitter_list");
+
+    buttonStart.addEventListener('click', () => {
+        if (!validation()) return false;
+        message.innerHTML = '';
+        result.innerHTML = '';
+        twitter_listdocument.style.display = "none";
+        getUserMeta(textUserName.value);
+    })
+
+    const getUserMeta = (userName) => {
+        const url = `${urlUserMetaAPI}?username=${userName}`
+        console.log(url);
+        fetch(url)
+            .then((res) => {
+                return res.json();
+            }).then((json) => {
+                if (json.error == true) {
+                    let text;
+                    text = `入力いただいたユーザ名から情報が取れませんでした…` + `</br>`;
+                    text += `</br>`;
+                    text += `ユーザ名は正しく入力されていますか？半角全角は正しいですか？IDに記号とか特殊な文字が入ってたりしますか？` + `</br>`;
+                    text += `※正しく打ち込んでいてもダメな人もいるみたいです…おのれエピック…` + `</br>`;
+                    text += `</br>`;
+                    text += `英語読める人用にエラーメッセージ出しておくね。` + `</br>`;
+                    text += `エラーコード:${json.errorCode}` + `</br>`;
+                    text += `エラー詳細:${json.errorMessage}` + `</br>`;
+                    message.innerHTML = text;
+                    message.appendChild(createHr());
+                } else {
+                    console.log(json);
+                    getUserStatus(json.uid);
+                }
+            }).catch( (err) => {
+                console.log(err);
+        });
+    };
+
+    const getUserStatus = (uid) => {
+        const url = `${urlUserStatusAPI}?user_id=${uid}`
+        console.log(url);
+        fetch(url)
+            .then((res) => {
+                return res.json();
+            }).then((json) => {
+                if (json.error) {
+                    let text;
+                    text = `ユーザ情報の取得はできたんだけど戦績は取得できませんでした…` + `</br>`;
+                    text += `おのれEpicGames` + `</br>`;
+                    text += `</br>`;
+                    text += `英語詳しい人用にエラーメッセージ出しておくね。` + `</br>`;
+                    text += `エラーコード:${json.error.code}` + `</br>`;
+                    text += `エラー詳細:${json.error.msg}` + `</br>`;
+                    message.innerHTML = text;
+                    result.appendChild(createHr());
+
+                } else {
+                    console.log(json);
+                    setData(json);
+                }
+            }).catch( (err) => {
+                console.log(err);
+        });
+    }
+
+    const setData = (json) => {
+        let data;
+        if (playstyle.value == 'gamepad') {
+            data = json.data.gamepad;
+        } else if (playstyle.value == 'keyboardmouse') {
+            data = json.data.keyboardmouse;
+        } else if (playstyle.value == 'touch') {
+            data = json.data.touch;
+        };
+        if (!data) { return setMessage('選んだ端末での戦闘データが無いみたいです…'); }
+        createSoloData(data);
+        createDuoData(data);
+        createSquadData(data);
+        twitter_listdocument.style.display = "block";
+    }
+    
+    const createSoloData = (data) => {
+        let div = createDiv('solo');
+        div.appendChild(createH2('ソロ'));
+        if (data.defaultsolo) {
+            div = createDivRuleData(div, data.defaultsolo.default);
+        } else {
+            div.appendChild(createP('ソロの戦闘データがありません。'));
+        }
+        result.appendChild(div);
+    }
+    
+    const createDuoData = (data) => {
+        let div = createDiv('duo');
+        div.appendChild(createH2('デュオ'));
+        if (data.defaultduo) {
+            div = createDivRuleData(div, data.defaultduo.default);
+        } else {
+            div.appendChild(createP('デュオの戦闘データがありません。'));
+        }
+        result.appendChild(div);
+    }
+    
+    const createSquadData = (data) => {
+        let div = createDiv('squad');
+        div.appendChild(createH2('スクアッド'));
+        if (data.defaultsquad) {
+            div = createDivRuleData(div, data.defaultsquad.default);
+        } else {
+            div.appendChild(createP('スクアッドの戦闘データがありません。'));
+        }
+        result.appendChild(div);
+    }
+
+    const createDivRuleData = (div, json) => {
+        // データない場合はnullとかかえってくるので0を入れてあげる
+        const matchesplayed = json.matchesplayed ? json.matchesplayed : 0;
+        const placetop1 = json.placetop1 ? json.placetop1 : 0;
+        const kills = json.kills ? json.kills : 0;
+        const killsDeath = roundSecondDecimal(kills / (matchesplayed - placetop1));
+        // 結果を生成
+        const killsGame = roundSecondDecimal(kills / matchesplayed);
+        div.appendChild(createP('プレイ数:' + matchesplayed));
+        div.appendChild(createP('ビクロイ数:' + placetop1));
+        div.appendChild(createP('キル数:' + kills));
+        div.appendChild(createP('キル/デス比:' + killsDeath));
+        div.appendChild(createP('キル/ゲーム比:' + killsGame));
+        div.appendChild(createHr());
+        return div;
+    }
+
+    const createDiv = (id) => {
+        const div = document.createElement('div');
+        div.id = 'result_' + id;
+        div.className = 'result_list'
+        return div;
+    }
+
+    const createP = (msg) => {
+        const p = document.createElement('p');
+        p.textContent = msg;
+        return p;
+    }
+
+    const createH2 = (msg) => {
+        const h2 = document.createElement('h2');
+        h2.textContent = msg;
+        return h2;
+    }
+
+    const createHr = () => {
+        const Hr = document.createElement('Hr');
+        return Hr;
+    }
+
+    const setMessage = (msg) => {
+        const text = msg + '</br>';
+        message.innerHTML = text;
+        result.appendChild(createHr());
+        return;
+    }
+    
+    const roundSecondDecimal = (number) => {
+        return Math.round(number * 100) / 100;
+    }
+
+    /**
+     * 入力チェックを行う。
+     * 問題があった場合はalert出してfalseを返す
+     * @return boolean
+     */
+    const validation = () => {
+        if (textUserName.value == '') return errorAlert('ユーザ名を入力してください');
+        if (playstyle.value == '') return errorAlert('端末を選択してください');
+        return true;
+    }
+
+    const errorAlert = (msg) => {
+        alert(msg);
+        return false;
+    }
+});
